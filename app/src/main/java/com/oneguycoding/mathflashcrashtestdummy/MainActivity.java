65f9256afb99aa4f7c1021c3a6b76d3d701aff1e
Steeve McCauley <steeve@oneguycoding.com>
2017-09-14 14:58:52 -0400

    * first stab at trying to use the google drive api to store files on drive

--- app/src/main/java/com/oneguycoding/mathflashcrashtestdummy/MainActivity.java
+++ app/src/main/java/com/oneguycoding/mathflashcrashtestdummy/MainActivity.java
@@ -1,11 +1,11 @@
 package com.oneguycoding.mathflashcrashtestdummy;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
+import android.content.IntentSender;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -17,21 +17,22 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.lang.reflect.Type;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+		GoogleApiClient.ConnectionCallbacks,
+		GoogleApiClient.OnConnectionFailedListener {
 	public static final String EXTRA_OPS = "ops";
 	public static final String EXTRA_USERDATA = "userdata";
 	public static final int RESULT_OPS = 100;
 	public static final int RESULT_USERDATA = 101;
+	public static final int RESOLVE_CONNECTION_REQUEST_CODE = 102;
 
 	private static final ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
 
@@ -48,15 +49,51 @@ public class MainActivity extends AppCompatActivity {
 	//private String curUser;
 	private UserResults userResults;
 	private String jsonFilename;
+	private GoogleApiClient mGoogleApiClient;
 
 	public MainActivity() {
 		jsonFilename = "MathFlashCrashTestDummy.json";
 	}
 
 	@Override
+	protected void onStart() {
+		super.onStart();
+		//mGoogleApiClient.connect();
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		if (connectionResult.hasResolution()) {
+			try {
+				connectionResult.startResolutionForResult(this, RESOLVE_CONNECTION_REQUEST_CODE);
+			} catch (IntentSender.SendIntentException e) {
+				// Unable to resolve, message user appropriately
+			}
+		} else {
+			GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), this, 0).show();
+		}
+	}
+
+	@Override
+	public void onConnected(@Nullable Bundle bundle) {
+
+	}
+
+	@Override
+	public void onConnectionSuspended(int i) {
+
+	}
+
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.addApi(Drive.API)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this)
+				.build();
 
 		if (userDataMap == null) {
 			userDataMap = new UserDataMap((String) getText(R.string.user_default));
@@ -263,6 +300,11 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		switch(requestCode) {
+			case RESOLVE_CONNECTION_REQUEST_CODE:
+				if (resultCode == RESULT_OK) {
+					mGoogleApiClient.connect();
+				}
+				break;
 			case RESULT_OPS:
 				if (resultCode == RESULT_OK) {
 					ops = (OperationsClass) intent.getSerializableExtra(EXTRA_OPS);
